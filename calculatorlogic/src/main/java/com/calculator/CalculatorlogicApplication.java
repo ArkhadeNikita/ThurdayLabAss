afe1e0abad9838021724a4aa4ce96e940241cf49
ArkhadeNikita <121220173+ArkhadeNikita@users.noreply.github.com>
2023-03-09 22:04:23 +0530

    Add files via upload

---
+++ calculatorlogic/src/main/java/com/calculator/CalculatorlogicApplication.java
@@ -0,0 +1,13 @@
+package com.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorlogicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorlogicApplication.class, args);
+	}
+
+}
